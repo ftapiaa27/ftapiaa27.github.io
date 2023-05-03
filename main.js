da7341e73fe58136cc575392976fa225274671bb
ftapiaa27@gmail.com <Fausto Tapia>
2023-05-03 09:20:53 -0600

    Add event listener to show or hide images

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const projectImages = document.getElementsByClassName('project-image');
-
+const projects = document.getElementsByClassName('project');
 for (let i = 1; i < projectImages.length; i++) {
     projectImages[i].style.display = 'none';
 }
-
+function showProjectImage(event) {
+    if (event.target.style.display === 'none') {
+        event.target.style.display = 'block';
+    } else {
+        event.target.style.display = 'none';
+    }
+}
+for (let i = 0; i < projects.length; i++) {
+    projects[i].addEventListener('click', showProjectImage);
+}
 const otherSkills = document.getElementById('second-skills-container');
 otherSkills.style.display = 'none';
+
