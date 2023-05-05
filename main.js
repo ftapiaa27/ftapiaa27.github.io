fe777025296b618138541cecda8aa20dd185d5d2
ftapiaa27@gmail.com <Fausto Tapia>
2023-05-05 00:14:36 -0600

    Add hide and show pics and responsiveness

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const projectImages = document.getElementsByClassName('project-image');
 const projects = document.getElementsByClassName('project');
-for (let i = 1; i < projectImages.length; i++) {
-    projectImages[i].style.display = 'none';
-}
-function showProjectImage(event) {
-    if (event.target.style.display === 'none') {
-        event.target.style.display = 'block';
-    } else {
-        event.target.style.display = 'none';
-    }
-}
+
 for (let i = 0; i < projects.length; i++) {
-    projects[i].addEventListener('click', showProjectImage);
+    projects[i].addEventListener('click', function() {
+        projectImages[i].style.display = 'flex';
+        document.getElementById("placeholder").style.display = 'none';
+        for (let j = 0; j < projects.length; j++) {
+            if (i !== j) {
+                projectImages[j].style.display = 'none';
+            }
+        }
+    });
 }
+
 const otherSkills = document.getElementById('second-skills-container');
-otherSkills.style.display = 'none';
+const otherSkillsTitle = document.getElementById('other-skills');
+let otherSkillsShown = false;
+
+otherSkillsTitle.addEventListener('click', function() {
+    if (otherSkillsShown) {
+        otherSkills.style.display = 'none';
+        otherSkillsShown = false;
+    } else {
+        otherSkills.style.display = 'flex';
+        otherSkillsShown = true;
+    }
+});
+
+
 
